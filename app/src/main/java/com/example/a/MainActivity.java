4e204f944cc0b1fab9d2c147588edfc96feab0c8
khj0603 <ekfvnddl99@naver.com>
2020-07-08 17:12:45 +0900

    mmm

--- app/src/main/java/com/example/a/MainActivity.java
+++ app/src/main/java/com/example/a/MainActivity.java
@@ -37,6 +37,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        System.out.println("Hello world");
     }
 }
