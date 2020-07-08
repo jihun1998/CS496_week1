ca040b906362df2aa4e75a6e72111fad324058c0
jihun1998 <jihun1998@kaist.ac.kr>
2020-07-08 17:16:37 +0900

    commit test

--- app/src/main/java/com/example/a/MainActivity.java
+++ app/src/main/java/com/example/a/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);git
+        TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
 
